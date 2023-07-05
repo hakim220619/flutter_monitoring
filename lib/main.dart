8d9a3478243d25bf2937c80a1eb55040a544e537
Dani lukman <danilukman2206@gmail.com>
2023-07-05 22:07:41 +0700

    monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:monitoring/home/view/home.dart';
+// ignore: unused_import
 import 'package:monitoring/login/view/logo.dart';
 
 
